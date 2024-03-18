f7bbbc590450debbab49c2812a200692cdde25c5
Gokul <gokulchandan24@gmail.com>
2024-03-18 18:15:51 +0530

    whole app done

---
+++ app/action.ts
@@ -0,0 +1,13 @@
+"use server";
+
+const fetchAnime = async (page: number) => {
+  const response = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
+  );
+
+  const data = await response.json();
+
+  return data;
+};
+
+export default fetchAnime;
